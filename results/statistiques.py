581133c410ddb8900aa53878ecfcc4308249df7d
Francepnadeau <fpaqu070@uottawa.ca>
2017-07-13 09:28:44 -0700

    Update statistiques.py

--- results/statistiques.py
+++ results/statistiques.py
@@ -19,7 +19,7 @@ import itertools
     
 # print(d)
 
-with open(5_1_results) as f:
+with open('5_1_results') as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content] 
