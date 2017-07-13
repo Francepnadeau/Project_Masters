da97e10ffd957eb7039d2e32f4dcd174e98bb483
Francepnadeau <fpaqu070@uottawa.ca>
2017-07-13 09:47:27 -0700

    Update statistiques.py

--- results/statistiques.py
+++ results/statistiques.py
@@ -24,6 +24,6 @@ with open('5_1_results') as f:
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content] 
 
-clean = [ x for x in content if x.isdigit() ]
+clean = filter(str.isdigit, content)
 
 print(clean)
